ea6ec72547f9ba734ffe2a49f32665be5e7ebc8f
Josh Diwa <47367644+joshdiwa@users.noreply.github.com>
2021-11-23 15:37:14 -0700

    Added Arduino Code File Used For Project.

---
+++ SoundLEDCode.ino
@@ -0,0 +1,82 @@
+// CPSC 581 PROJECT 3 NOISE DETECTOR LED CODE
+// Created by Josh Diwa, Ishan Vyas, Carl Lyss, Noah Bensler
+// SOURCE CODE ADAPTED FROM: https://62e1a3e9-c5ed-4f1a-8182-14ebd7c76947.filesusr.com/ugd/22a975_8c6883be82844b05a6b4e4db55373c73.pdf
+
+// RESISTORS = We used the one from our Arduino Kits.
+// M/F Wires = We used the one from our Arduino Kits.
+// LEDs are from Neil's Arudino Kit.
+
+int digitalOutput = 2; // Digital Output Pin
+int analogOutput = A0; // Analog Output Pin (Noise Sensor)
+int soundLevelLow = 80;
+int soundLevelMid = 90;
+int soundLevelMidHigh = 100;
+int soundLevelHigh = 110;
+int soundLevelXtreme = 120;
+int noiseLevelValue = 0; // Used to track noise level.
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+  noiseLevelValue = analogRead(analogOutput); // Read Analog Value.
+  //Serial.print("Analog: ");
+  //Serial.print(noiseLevelValue);
+  //Serial.print(" ");
+    //Serial.print("Digital: ");
+  //Serial.println(digitalRead(digitalOutput));
+
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  Serial.println(noiseLevelValue);
+
+  if (noiseLevelValue >= soundLevelLow) {
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, HIGH);
+    if (noiseLevelValue >= soundLevelMid) {
+      digitalWrite(8, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+      }
+    if (noiseLevelValue >= soundLevelMidHigh) {
+      digitalWrite(8, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+     }
+
+    if (noiseLevelValue >= soundLevelHigh) {
+      digitalWrite(8, LOW);
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+      }
+
+    if (noiseLevelValue >= soundLevelXtreme) {
+      digitalWrite(8, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, HIGH);
+      }
+    }
+
+    
+  }
